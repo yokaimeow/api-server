b1799d3a983cd1358252db6a043d23f1843bdc95
yokaimeow <yokaimeow@qq.com>
2021-12-22 20:55:00 +0800

    feat: add logrus

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the info severity or above.
+	log.SetLevel(log.InfoLevel)
+}
 
 func main() {
+	log.Info("IOJelly system start...")
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
